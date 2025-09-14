f7b81c9b458148601963d0ffdbc6af9d06f5c52d
sankar1928 <Sankar@lyrostech.com>
2025-09-14 15:57:53 +0530

    Update addition.py

--- addition.py
+++ addition.py
@@ -1,8 +1,6 @@
 def add(a, b):
     """Function to add two numbers"""
     return a + b
-
-
 if __name__ == "__main__":
     num1 = float(input("Enter the first number: "))
     num2 = float(input("Enter the second number: "))

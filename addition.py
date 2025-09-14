18781afb936cafdd70dc5ea1310121b12ead72d4
Lakshmi Sankar Pabbisetty <sankar@lyrostech.com>
2025-09-14 15:23:52 +0530

    adding python file

---
+++ addition.py
@@ -0,0 +1,7 @@
+def add(a, b):
+    """Function to add two numbers"""
+    return a + b
+if __name__ == "__main__":
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+    print("The sum is:", add(num1, num2))
